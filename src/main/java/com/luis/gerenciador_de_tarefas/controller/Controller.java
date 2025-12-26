42e6705b6684329b142eb487e32cf5eb91520872
Luis Jackson <luisjacksonjr@gmail.com>
2025-12-26 13:02:20 -0300

    feat: add pasta controller e teste inicial

---
+++ src/main/java/com/luis/gerenciador_de_tarefas/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.luis.gerenciador_de_tarefas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Controller
+@RequestMapping // Define a rota base que os metodos desta classe vão seguir
+public class Controller {
+
+        @GetMapping("/iniciar") // Mapeia a requisição http get para esse metodo
+        public String iniciar(){
+            return "Hello World";
+        }
+
+}
