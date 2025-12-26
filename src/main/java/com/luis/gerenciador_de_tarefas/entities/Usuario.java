267481c7c603416fa98efe509b1025416358803a
Luis Jackson <luisjacksonjr@gmail.com>
2025-12-26 14:04:58 -0300

    feat: add package entities / diagrama-inicial

---
+++ src/main/java/com/luis/gerenciador_de_tarefas/entities/Usuario.java
@@ -0,0 +1,62 @@
+package com.luis.gerenciador_de_tarefas.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // Transforma a classe em uma entidade do DB
+@Table(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment do DB
+
+    Long idUsuario;
+
+    private String email;
+    private String senha;
+    private String nome;
+    private String telefone;
+
+    public Usuario(String email, String senha, String nome, String telefone) {
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Usuario() {
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+   
+    
+}
