0b6e1c5b2b0a6c153907cd137119afbf20ab0260
Luis Jackson <luisjacksonjr@gmail.com>
2025-12-26 21:33:39 -0300

    refactor: Alterado construtores e get/set para padrão lombok

--- src/main/java/com/luis/gerenciador_de_tarefas/Tarefa/TarefaModel.java
+++ src/main/java/com/luis/gerenciador_de_tarefas/Tarefa/TarefaModel.java
@@ -11,7 +11,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Tarefa")
 public class TarefaModel {
@@ -29,85 +35,5 @@ public class TarefaModel {
     @ManyToOne // Muitas tarefas podem pertencer a um único usuário
     @JoinColumn(name = "Usuario_idUsuario") // FK
     private UsuarioModel usuario; 
-
-    public TarefaModel(String titulo, String descricao, String status, LocalDate data_inicio, LocalDate data_final) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.status = status;
-        this.data_inicio = data_inicio;
-        this.data_final = data_final;
-    }
-
-
-    public Long getIdTarefa() {
-        return idTarefa;
-    }
-
-
-    public void setIdTarefa(Long idTarefa) {
-        this.idTarefa = idTarefa;
-    }
-
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-
-    public String getStatus() {
-        return status;
-    }
-
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-
-    public LocalDate getData_inicio() {
-        return data_inicio;
-    }
-
-
-    public void setData_inicio(LocalDate data_inicio) {
-        this.data_inicio = data_inicio;
-    }
-
-
-    public LocalDate getData_final() {
-        return data_final;
-    }
-
-
-    public void setData_final(LocalDate data_final) {
-            this.data_final = data_final;
-        }
-
-
-    public UsuarioModel getUsuario() { 
-        return usuario; 
-    }
-
-    public void setUsuario(UsuarioModel usuario) { 
-        this.usuario = usuario; 
-    }
-
-    
-    
 
 }
--- src/main/java/com/luis/gerenciador_de_tarefas/Usuario/UsuarioModel.java
+++ src/main/java/com/luis/gerenciador_de_tarefas/Usuario/UsuarioModel.java
@@ -4,69 +4,36 @@ import java.util.List;
 
 import com.luis.gerenciador_de_tarefas.Tarefa.TarefaModel;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity // Transforma a classe em uma entidade do DB
 @Table(name = "Usuario")
+@NoArgsConstructor // Criar construtor com lombok
+@AllArgsConstructor
+@Data // Get e Set Com lombok
 public class UsuarioModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment do DB
-
     Long idUsuario;
 
+    @Column(unique=true)
     private String email;
     private String senha;
     private String nome;
     private String telefone;
 
-
     @OneToMany(mappedBy="usuario") // Um usuario pode ter varias tarefas
     private List<TarefaModel> tarefas;
 
-    
-    public UsuarioModel(String email, String senha, String nome, String telefone) {
-        this.email = email;
-        this.senha = senha;
-        this.nome = nome;
-        this.telefone = telefone;
-    }
-
-    public UsuarioModel() {
-    }
-
-
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getSenha() {
-        return senha;
-    }
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-   
     
 }
