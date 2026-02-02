58c33d761325600676ae217027c5bdcac1f9a13f
Luis Jackson <luisjacksonjr@gmail.com>
2026-02-01 22:14:46 -0300

    feat: Feito injeção de dependencias service/repository controller/service, e add listar tarefas service

--- src/main/java/com/luis/gerenciador_de_tarefas/controller/TarefaController.java
+++ src/main/java/com/luis/gerenciador_de_tarefas/controller/TarefaController.java
@@ -1,5 +1,7 @@
 package com.luis.gerenciador_de_tarefas.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +9,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luis.gerenciador_de_tarefas.model.TarefaModel;
+import com.luis.gerenciador_de_tarefas.service.TarefaService;
+
 @RestController
 @RequestMapping // Define a rota base que os metodos desta classe vão seguir
 public class TarefaController {
+
+    // Injeção de dependencia do service p se comunicar com a camada de serviço 
+    private TarefaService tarefaService;
+    public TarefaController(TarefaService tarefaService){
+        this.tarefaService = tarefaService;
+    }
 
     //Adicionar 
     @PostMapping("/criar")
@@ -19,8 +30,8 @@ public class TarefaController {
     
     // Mostrar
     @GetMapping("/todas")
-    public String mostrarTodasTarefas(){
-        return "Mostrar Tarefa";
+    public List<TarefaModel> mostrarTodasTarefas(){
+        return tarefaService.mostrarTodasTarefas();
     }
 
     // Mostrar tarefa por ID
--- src/main/java/com/luis/gerenciador_de_tarefas/service/TarefaService.java
+++ src/main/java/com/luis/gerenciador_de_tarefas/service/TarefaService.java
@@ -1,5 +1,24 @@
 package com.luis.gerenciador_de_tarefas.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.luis.gerenciador_de_tarefas.model.TarefaModel;
+import com.luis.gerenciador_de_tarefas.repository.TarefaRepository;
+
+@Service
 public class TarefaService {
+
+    private TarefaRepository tarefaRepository; // Injetar dependencia para Fazer comunicação do service com repository
+
+    public TarefaService(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    // Listar todas as tarefas
+    public List<TarefaModel> mostrarTodasTarefas(){
+        return tarefaRepository.findAll();
+    }
 
 }
